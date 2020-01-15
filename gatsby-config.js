327faaa3dd91b4936ef5fb38c72f9c59029812d3
trainto <trainto@gmail.com>
2020-01-15 14:31:54 +0900

    Add feed options

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,12 +3,12 @@ module.exports = {
     title: `Trainto.log()`,
     author: `Hakjoon Sim`,
     description: `Hakjoon's blog`,
-    siteUrl: `https://blog.trainto.me/`,
+    siteUrl: `https://trainto.me/`,
     social: {
       twitter: 'trainto',
       linkedin: 'hakjoon-sim',
       github: 'trainto'
-    },
+    }
   },
   plugins: [
     {
@@ -17,14 +17,14 @@ module.exports = {
         path: `${__dirname}/content/blog`,
         name: `blog`,
         ignore: [`**/WIP*`]
-      },
+      }
     },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
         path: `${__dirname}/content/assets`,
-        name: `assets`,
-      },
+        name: `assets`
+      }
     },
     {
       resolve: `gatsby-transformer-remark`,
@@ -33,14 +33,14 @@ module.exports = {
           {
             resolve: `gatsby-remark-images`,
             options: {
-              maxWidth: 590,
-            },
+              maxWidth: 590
+            }
           },
           `gatsby-remark-prismjs`,
           `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
+          `gatsby-remark-smartypants`
+        ]
+      }
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
@@ -48,9 +48,61 @@ module.exports = {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: 'UA-73310674-3'
+      }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+                site_url: siteUrl
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => {
+              return allMarkdownRemark.edges.map(edge => {
+                return Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.excerpt,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                });
+              });
+            },
+            query: `
+              {
+                allMarkdownRemark(
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                ) {
+                  edges {
+                    node {
+                      excerpt
+                      html
+                      fields { slug }
+                      frontmatter {
+                        title
+                        date
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml',
+            title: 'Trainto.log()'
+          },
+        ],
       },
     },
-    `gatsby-plugin-feed`,
     // {
     //   resolve: `gatsby-plugin-manifest`,
     //   options: {
@@ -63,7 +115,7 @@ module.exports = {
     //     icon: `content/assets/gatsby-icon.png`,
     //   },
     // },
-    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-helmet`
     // {
     //   resolve: `gatsby-plugin-typography`,
     //   options: {
@@ -73,5 +125,5 @@ module.exports = {
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
-  ],
-}
+  ]
+};
