0d76b9ed9b7a36a14fdc237fcbde29a07eda37f4
trainto <trainto@gmail.com>
2020-01-15 17:23:46 +0900

    Add manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -103,18 +103,18 @@ module.exports = {
         ],
       },
     },
-    // {
-    //   resolve: `gatsby-plugin-manifest`,
-    //   options: {
-    //     name: `Gatsby Starter Blog`,
-    //     short_name: `GatsbyJS`,
-    //     start_url: `/`,
-    //     background_color: `#ffffff`,
-    //     theme_color: `#663399`,
-    //     display: `minimal-ui`,
-    //     icon: `content/assets/gatsby-icon.png`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Trainto.log()`,
+        short_name: `Trainto`,
+        start_url: `/`,
+        background_color: `#282c35`,
+        theme_color: `#e31b6d`,
+        display: `standalone`,
+        icon: `content/assets/icon.png`,
+      },
+    },
     `gatsby-plugin-react-helmet`
     // {
     //   resolve: `gatsby-plugin-typography`,
