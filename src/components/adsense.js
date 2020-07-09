ecc6d3569168259c132fa7ee0ee49f3bdbc40310
trainto <trainto@gmail.com>
2020-07-09 18:47:20 +0900

    Place adsense script to head

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -2,35 +2,21 @@ import React, { useEffect } from 'react';
 
 const Adsense = props => {
   useEffect(() => {
-    const script = document.createElement('script');
-    script.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js';
-    script.async = true;
-
-    document.body.appendChild(script);
-
-    setTimeout(() => {
-      const adsbygoogle = window.adsbygoogle || [];
-      adsbygoogle.push({});
-    }, 300);
+    const adsbygoogle = window.adsbygoogle || [];
+    adsbygoogle.push({});
   }, []);
 
   return (
-    <>
-      {/* <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> */}
-
-      <ins
-        className="adsbygoogle"
-        style={props.style ? props.style : { display: 'block' }}
-        data-ad-client="ca-pub-6978978720477594"
-        data-ad-slot={props.slot}
-        data-ad-layout={props.layout ? props.layout : null}
-        data-ad-format={props.format}
-        data-full-width-responsive={props.responsive ? 'true' : 'false'}
-        data-ad-layout-key={props.layout_key ? props.layout_key : null}
-      />
-
-      {/* <script>(adsbygoogle = window.adsbygoogle || []).push({});</script> */}
-    </>
+    <ins
+      className="adsbygoogle"
+      style={props.style ? props.style : { display: 'block' }}
+      data-ad-client="ca-pub-6978978720477594"
+      data-ad-slot={props.slot}
+      data-ad-layout={props.layout ? props.layout : null}
+      data-ad-format={props.format}
+      data-full-width-responsive={props.responsive ? 'true' : 'false'}
+      data-ad-layout-key={props.layout_key ? props.layout_key : null}
+    />
   );
 };
 
