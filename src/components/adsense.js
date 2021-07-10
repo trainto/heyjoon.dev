edae1e30dc7b8edf1f39b580add6185598170976
Hakjoon Sim <trainto@gmail.com>
2021-07-11 02:42:05 +0900

    fix: adsense code into dangerouslySetInnerHTML

--- src/components/adsense.js
+++ src/components/adsense.js
@@ -1,22 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
 const Adsense = props => {
-  useEffect(() => {
-    const adsbygoogle = window.adsbygoogle || [];
-    adsbygoogle.push({});
-  }, []);
-
   return (
-    <ins
-      className="adsbygoogle"
-      style={props.style ? props.style : { display: 'block' }}
-      data-ad-client="ca-pub-6978978720477594"
-      data-ad-slot={props.slot}
-      data-ad-layout={props.layout ? props.layout : null}
-      data-ad-format={props.format}
-      data-full-width-responsive={props.responsive ? 'true' : 'false'}
-      data-ad-layout-key={props.layout_key ? props.layout_key : null}
-    />
+    <>
+      <ins
+        className="adsbygoogle"
+        style={props.style ? props.style : { display: 'block' }}
+        data-ad-client="ca-pub-6978978720477594"
+        data-ad-slot={props.slot}
+        data-ad-layout={props.layout ? props.layout : null}
+        data-ad-format={props.format}
+        data-full-width-responsive={props.responsive ? 'true' : 'false'}
+        data-ad-layout-key={props.layout_key ? props.layout_key : null}
+      />
+       <script dangerouslySetInnerHTML={{ __html: `(adsbygoogle = window.adsbygoogle || []).push({});` }} />
+    </>
   );
 };
 
