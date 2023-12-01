591ac6d50fa42f0312ac545b424d40a76cd234e0
Hakjoon Sim <hakjoon.sim@sk.com>
2023-12-01 19:16:56 +0900

    Add profile image

--- src/app/components/me.tsx
+++ src/app/components/me.tsx
@@ -3,7 +3,10 @@ import Image from 'next/image';
 export default function Me() {
   return (
     <div className="text-gray-400 sm:border-r border-gray-500 pb-10 sm:sticky sm:top-10 px-5 sm:px-10">
-      <div className="text-2xl text-gray-300">Hakjoon Sim</div>
+      <a href="https://you.heyjoon.dev">
+        <Image src="/me.jpg" className="rounded-full" width={100} height={100} alt="Linkedin" />
+      </a>
+      <div className="text-2xl text-gray-300 mt-5">Hakjoon Sim</div>
       <div>Developer who lives in Seoul,</div>
       <div>like programming and doing nothing.</div>
       <div className="mt-5 flex space-x-2">
