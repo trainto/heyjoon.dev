26c9b5d4ae4cf2b24c6e5f07019fe49a7dc3140a
Hakjoon Sim <trainto@gmail.com>
2023-12-02 16:35:08 +0900

    Add navigation to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,15 +7,39 @@ const Header = () => {
   const pathname = usePathname();
 
   return (
-    <header className="text-right px-5">
-      <Link href="/">
-        <div
-          className={`text-4xl pt-10 font-bold ${
-            pathname === '/' ? 'text-gray-300-animation' : 'text-brand1-animation'
-          }`}
-        >
-          Joon.log()
-        </div>
+    <header className="flex justify-end items-end space-x-5 px-5">
+      <nav>
+        <span>
+          <Link
+            href="/about"
+            className={`${
+              pathname.startsWith('/about') ? 'text-brand2-animation' : 'text-gray-300-animation'
+            }`}
+          >
+            About
+          </Link>
+        </span>
+        <span className="text-xs text-gray-500 px-2">|</span>
+        <span>
+          <Link
+            href="/"
+            className={`${
+              pathname.startsWith('/posts') || pathname === '/'
+                ? 'text-brand2-animation'
+                : 'text-gray-300-animation'
+            }`}
+          >
+            Blog
+          </Link>
+        </span>
+      </nav>
+      <Link
+        href="/"
+        className={`text-4xl pt-10 font-bold ${
+          pathname === '/' ? 'text-gray-300-animation' : 'text-brand1-animation'
+        }`}
+      >
+        Joon.log()
       </Link>
     </header>
   );
