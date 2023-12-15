b65ec6c8061e41d4ae00423e2edcf1902a4f4e09
Hakjoon Sim <hakjoon.sim@sk.com>
2023-12-15 21:49:27 +0900

    Add loading component

--- src/components/places/side.tsx
+++ src/components/places/side.tsx
@@ -3,13 +3,20 @@
 import { fetcher } from '@/lib/api/fetchers';
 import useSWR from 'swr';
 import Tag from './tag';
+import Loading from './loading';
 
 const Side = () => {
-  const { data: topTags } = useSWR<string[]>('/places/tags/top?limit=5', fetcher);
+  const { data: topTags, isLoading } = useSWR<string[]>('/places/tags/top?limit=5', fetcher);
+
   return (
     <div className="sm:sticky sm:top-10 border border-gray-700 p-3">
       <div className="text-xl font-bold">Top tags:</div>
-      <div>
+      {isLoading && (
+        <div className="flex justify-center mt-2">
+          <Loading />
+        </div>
+      )}
+      <div className="mt-1 px-1">
         {topTags?.map((t) => (
           <div key={t}>
             <Tag tag={'#' + t} />
