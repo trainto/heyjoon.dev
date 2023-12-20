5069239c23414417caaddeaa35d68cd48a9a4583
Hakjoon Sim <hakjoon.sim@sk.com>
2023-12-20 18:36:34 +0900

    Add some info to User

--- src/components/places/user.tsx
+++ src/components/places/user.tsx
@@ -5,6 +5,7 @@ import { useCallback } from 'react';
 import { sendRequest } from '@/lib/api/fetchers';
 import { Storage } from '@/lib/utils-client';
 import { mutate } from 'swr';
+import { format } from 'date-fns';
 
 const User = () => {
   const { value: userInfo, dispatch: dispatchUserInfo } = useStore('userInfo');
@@ -27,10 +28,18 @@ const User = () => {
     <>
       <div className="hidden sm:block">
         <div className="flex justify-center">
-          <Avatar size={72} src={userInfo.avatar} />
+          <Avatar size={92} src={userInfo.avatar} />
         </div>
-        <div className="flex justify-center mt-3">
+        <div className="flex justify-end mt-3">
           <div className="text-sm">@{userInfo.nickname}</div>
+        </div>
+        {userInfo.intro && (
+          <div className="flex justify-end mt-2 text-gray-400 text-sm">
+            <div>{userInfo.intro}</div>
+          </div>
+        )}
+        <div className="flex justify-end mt-1 text-gray-500 text-sm">
+          Since: {format(new Date(userInfo.createdAt), 'MMM dd, yyyy')}
         </div>
         <div className="flex justify-end mt-3">
           <Button color="zinc" size="xs" onClick={onSignout}>
