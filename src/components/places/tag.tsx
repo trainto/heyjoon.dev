3c55ce8aad68f168d29d024c9da468e505bbeb8c
Hakjoon Sim <hakjoon.sim@sk.com>
2023-12-21 10:27:39 +0900

    Do not prefecth every tag

--- src/components/places/tag.tsx
+++ src/components/places/tag.tsx
@@ -2,7 +2,11 @@ import Link from 'next/link';
 import { memo } from 'react';
 
 function Tag({ tag }: { tag: string }) {
-  return <Link href={`/places/tag/${tag.replace(/#/g, '')}`}>{tag}</Link>;
+  return (
+    <Link href={`/places/tag/${tag.replace(/#/g, '')}`} prefetch={false}>
+      {tag}
+    </Link>
+  );
 }
 
 export default memo(Tag);
