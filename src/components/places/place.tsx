3bfbd08a95348d2f841cc58b67e78e5217f5281a
Hakjoon Sim <hakjoon.sim@sk.com>
2023-12-26 14:27:25 +0900

    Swiper pagination positino adjusted

--- src/components/places/place.tsx
+++ src/components/places/place.tsx
@@ -1,5 +1,5 @@
 import { format } from 'date-fns';
-import { memo, useMemo } from 'react';
+import { memo, useMemo, useRef } from 'react';
 import Tag from './tag';
 import Image from 'next/image';
 import Avatar from './avatar';
@@ -33,9 +33,13 @@ const Place = ({ place }: { place: Place }) => {
         <div className="text-xs text-gray-500">{createdAt}</div>
       </div>
 
-      <div className="relative bg-black w-full pt-100 my-2 rounded-md">
+      <div className="relative bg-black w-full pt-100 mt-2 rounded-md">
         <div className="place-images absolute top-0 right-0 bottom-0 left-0 grid content-center">
-          <Swiper modules={[Pagination]} slidesPerView={1} pagination={{ clickable: true }}>
+          <Swiper
+            modules={[Pagination]}
+            slidesPerView={1}
+            pagination={{ clickable: true, el: `.swiper-pagination-container-${place.id}` }}
+          >
             {images.map((src) => (
               <SwiperSlide key={src.split('.')[0]}>
                 <Image
@@ -51,7 +55,11 @@ const Place = ({ place }: { place: Place }) => {
           </Swiper>
         </div>
       </div>
-      <div className="px-1 text-sm">{built}</div>
+      <div
+        className={`swiper-pagination-container-${place.id} swiper-pagination swiper-pagination-clickable swiper-pagination-horizontal`}
+      ></div>
+
+      <div className="px-1 text-sm my-2">{built}</div>
     </div>
   );
 };
