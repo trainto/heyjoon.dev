50632f1bb30b4766d8783c5c1a6dd6c9811378a4
Hakjoon Sim <trainto@gmail.com>
2024-01-16 18:43:15 +0900

    PWA send /places after signin

--- src/app/places/auth/google-auth/page.tsx
+++ src/app/places/auth/google-auth/page.tsx
@@ -2,10 +2,12 @@
 
 import Loading from '@/components/places/loading';
 import { signin } from '@/lib/api';
+import { useRouter } from 'next/navigation';
 import { useCallback, useEffect, useRef } from 'react';
 
 export default function GoogleAuth() {
   const doneRef = useRef(false);
+  const router = useRouter();
 
   const signIn = useCallback(async (token: string) => {
     await signin(token);
@@ -30,10 +32,11 @@ export default function GoogleAuth() {
         window.close();
       } else {
         // Redirected to PWA case
+        router.replace('/places');
         signIn(token);
       }
     }
-  }, [signIn]);
+  }, [router, signIn]);
 
   return (
     <div className="fixed h-screen w-screen bg-black top-0 left-0 z-50 grid place-content-center">
