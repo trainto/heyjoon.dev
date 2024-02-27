9dfa7f226810efa422dc5ec2984b929db0a3f0da
Hakjoon Sim <trainto@gmail.com>
2024-02-27 17:24:30 +0900

    bp page layout adjusted

--- src/app/bp/page.tsx
+++ src/app/bp/page.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import Button from '@/components/button';
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export default function BP() {
   const [systolic, setSystolic] = useState<number>();
   const [diastolic, setDiastolic] = useState<number>();
 
+  const onConfirm = useCallback(() => {}, []);
+
   return (
     <>
-      <div className="flex space-x-5 px-14 items-end mt-40">
+      <div className="flex space-x-5 px-14 items-end justify-center mt-40">
         <div className="flex items-end space-x-2">
           <input
             type="number"
@@ -19,20 +21,22 @@ export default function BP() {
           />
         </div>
 
-        <div>/</div>
+        <div className="text-gray-400">/</div>
 
         <div className="flex justify-end items-end space-x-2">
           <input
             type="number"
             className="text-center bg-bg-main border-b w-20 text-3xl p-1 outline-none"
+            value={diastolic}
+            onChange={(e) => setDiastolic(parseInt(e.target.value))}
           />
         </div>
 
-        <div className="text-gray-500">mmHg</div>
+        <div className="text-gray-600 text-sm">mmHg</div>
       </div>
 
       <div className="text-end pt-10 pr-10 mb-40">
-        <Button color="indigo" size="lg" onClick={() => null}>
+        <Button color="indigo" size="lg" onClick={onConfirm}>
           Confirm
         </Button>
       </div>
