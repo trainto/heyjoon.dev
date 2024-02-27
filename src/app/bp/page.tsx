3da7490311a23d58a02161f65d1cf6450960d301
Hakjoon Sim <trainto@gmail.com>
2024-02-27 17:18:40 +0900

    feat: bp input page added

---
+++ src/app/bp/page.tsx
@@ -0,0 +1,41 @@
+'use client';
+
+import Button from '@/components/button';
+import { useState } from 'react';
+
+export default function BP() {
+  const [systolic, setSystolic] = useState<number>();
+  const [diastolic, setDiastolic] = useState<number>();
+
+  return (
+    <>
+      <div className="flex space-x-5 px-14 items-end mt-40">
+        <div className="flex items-end space-x-2">
+          <input
+            type="number"
+            className="text-center bg-bg-main border-b w-20 text-3xl p-1 outline-none"
+            value={systolic}
+            onChange={(e) => setSystolic(parseInt(e.target.value))}
+          />
+        </div>
+
+        <div>/</div>
+
+        <div className="flex justify-end items-end space-x-2">
+          <input
+            type="number"
+            className="text-center bg-bg-main border-b w-20 text-3xl p-1 outline-none"
+          />
+        </div>
+
+        <div className="text-gray-500">mmHg</div>
+      </div>
+
+      <div className="text-end pt-10 pr-10 mb-40">
+        <Button color="indigo" size="lg" onClick={() => null}>
+          Confirm
+        </Button>
+      </div>
+    </>
+  );
+}
