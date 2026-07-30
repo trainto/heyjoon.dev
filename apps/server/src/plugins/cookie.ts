a3d490686e0e614abd3449f4e36ef9f7e37896cb
Hakjoon Sim <trainto@gmail.com>
2026-07-30 12:34:04 +0900

    Removed server app

--- apps/server/src/plugins/cookie.ts
+++
@@ -1,6 +0,0 @@
-import cookie from '@fastify/cookie';
-import fp from 'fastify-plugin';
-
-export default fp(async (app) => {
-  app.register(cookie);
-});
--- apps/server/src/routes/github/index.ts
+++
@@ -1,7 +0,0 @@
-import { FastifyInstance } from 'fastify';
-
-export default async function githubRoutes(app: FastifyInstance) {
-  app.get('/pr', async () => {
-    // GET /github/pr
-  });
-}
